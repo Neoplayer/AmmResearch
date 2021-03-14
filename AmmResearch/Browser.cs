c1c0010691d295789216df4757da16dd87124119
Rostislav <therostikru@gmail.com>
2021-03-14 03:22:13 +0300

    Add project files.

---
+++ AmmResearch/Browser.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AmmResearch
+{
+    class Browser : IDisposable
+    {
+        internal const byte MaxTries = 5;
+        private const byte MaxConnections = 10; // Defines maximum number of connections per ServicePoint. Be careful, as it also defines maximum number of sockets in CLOSE_WAIT state
+        private const byte MaxIdleTime = 15; // Defines in seconds, how long socket is allowed to stay in CLOSE_WAIT state after there are no connections to it
+        internal readonly CookieContainer CookieContainer = new CookieContainer();
+        private HttpClient HttpClient;
+        internal Browser()
+        {
+            HttpClientHandler httpClientHandler = new HttpClientHandler
+            {
+                AllowAutoRedirect = true, // This must be false if we want to handle custom redirection schemes such as "steammobile"
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+                CookieContainer = CookieContainer,
+                MaxConnectionsPerServer = MaxConnections,
+                UseProxy = false
+            };
+            HttpClient = new HttpClient(httpClientHandler) { Timeout = TimeSpan.FromSeconds(60) };
+            HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Sutox/0.1");
+        }
+        public void Dispose() => HttpClient.Dispose();
+        internal Task<HttpResponseMessage> UrlPostToHttp(string requestUri, IReadOnlyCollection<KeyValuePair<string, string>> data = null, string referer = null)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            if (data != null)
+            {
+                try
+                {
+                    request.Content = new FormUrlEncodedContent(data);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            if (!string.IsNullOrEmpty(referer))
+            {
+                request.Headers.Referrer = new Uri(referer);
+            }
+            string json = JsonConvert.SerializeObject(request, Formatting.Indented);
+            var result = HttpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+
+            return result;
+        }
+        internal Task<string> UrlPostToString(string requestUri, IReadOnlyCollection<KeyValuePair<string, string>> data = null, string referer = null)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            if (data != null)
+            {
+                try
+                {
+                    request.Content = new FormUrlEncodedContent(data);
+                }
+                catch (UriFormatException e)
+                {
+                    Console.WriteLine($"{ToString()} {e.StackTrace}");
+                }
+            }
+            if (!string.IsNullOrEmpty(referer))
+            {
+                request.Headers.Referrer = new Uri(referer);
+            }
+            return HttpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead).Result.Content.ReadAsStringAsync();
+        }
+        internal Task<HttpResponseMessage> UrlGetToHttp(string request, string referer = null)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    HttpRequestMessage requestMsg = new HttpRequestMessage(HttpMethod.Get, request);
+                    if (!string.IsNullOrEmpty(referer))
+                    {
+                        requestMsg.Headers.Referrer = new Uri(referer);
+                    }
+                    HttpResponseMessage httpRespMsg = HttpClient.SendAsync(requestMsg, HttpCompletionOption.ResponseContentRead).Result;
+                    return httpRespMsg;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+            });
+        }
+        internal Task<string> UrlGetToString(string request, string referer = null)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    HttpRequestMessage requestMsg = new HttpRequestMessage(HttpMethod.Get, request);
+                    if (!string.IsNullOrEmpty(referer))
+                    {
+                        requestMsg.Headers.Referrer = new Uri(referer);
+                    }
+                    var httpRespMsg = HttpClient.SendAsync(requestMsg, HttpCompletionOption.ResponseContentRead).Result;
+                    string content = httpRespMsg.Content.ReadAsStringAsync().Result;
+                    return content;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    return null;
+                }
+            });
+        }
+    }
+}
---
+++ AmmResearch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmmResearch
+{
+    class Program
+    {
+        static void Main()
+        {
+            AmmPair pair = new AmmPair("SCRT", "USDT", 41171.48M, 131611.94M);
+            Console.WriteLine($"{pair.Ticker}={pair.Price}");
+            pair.CalculateIoForPrice(3.40M, out decimal pooledBaseDiff, out decimal pooledQuoteDiff);
+            Console.WriteLine($"diff {pair.BaseName} = {pooledBaseDiff} diff {pair.QuoteName} ={pooledQuoteDiff}");
+            decimal amountAdd = pooledQuoteDiff;
+            Console.WriteLine($"Added {amountAdd:0.000} {pair.QuoteName} into {pair.Ticker} pool");
+            decimal baseReturn = pair.AddQuote(amountAdd);
+            Console.WriteLine($"{pair.Ticker}={pair.Price}  return={baseReturn} {pair.BaseName}");
+        }
+    }
+    public class AmmPair
+    {
+        private decimal FeeRate { get; set; }
+        public string BaseName { get; set; }
+        public string QuoteName { get; set; }
+        public string Ticker => BaseName + QuoteName;
+        public decimal AmountBase { get; set; }
+        public decimal AmountQuote { get; set; }
+        public decimal K { get; set; }
+        public decimal Price => AmountQuote / AmountBase;
+        public AmmPair(string baseName, string quoteName, decimal amountBase, decimal amountQuote)
+        {
+            BaseName = baseName;
+            QuoteName = quoteName;
+            AmountBase = amountBase;
+            AmountQuote = amountQuote;
+            K = AmountBase * AmountQuote;
+            FeeRate = 0.003M;
+        }
+        public decimal AddBase(decimal addedBaseAmount)
+        {
+            if (addedBaseAmount < 0M)
+                if (Math.Abs(addedBaseAmount) >= AmountBase)
+                    throw new ArgumentOutOfRangeException();
+            AmountBase += addedBaseAmount;
+            decimal newAmountQuote = K / AmountBase;
+            decimal wipedQuoteAmount = AmountQuote - newAmountQuote;
+            AmountQuote = newAmountQuote;
+            return wipedQuoteAmount / (1M + FeeRate);
+        }
+        public decimal AddQuote(decimal addedQuoteAmount)
+        {
+            if (addedQuoteAmount < 0M)
+                if (Math.Abs(addedQuoteAmount) > AmountQuote)
+                    throw new ArgumentOutOfRangeException();
+            AmountQuote += addedQuoteAmount;
+            decimal newAmountBase = K / AmountQuote;
+            decimal wipedBaseAmount = AmountBase - newAmountBase;
+            AmountBase = newAmountBase;
+            return wipedBaseAmount / (1M + FeeRate);
+        }
+        public void CalculateIoForPrice(decimal price, out decimal diffBase, out decimal diffQuote)
+        {
+            if (price <= 0M)
+                throw new FormatException();
+            decimal newAmountQuote = (decimal)Math.Sqrt((double)(K * price));
+            decimal newAmountBase = (decimal)Math.Sqrt((double)(K / price));
+            diffBase = newAmountBase - AmountBase;
+            diffQuote = newAmountQuote - AmountQuote;
+        }
+    }
+}
